94c2a465ae07dc1fe91fdab3273106d39a2c7a53
xuxu <xu2983567126@126.com>
2026-08-13 17:45:15 +0800

    完成lab09

--- labs/lab09/lab09.scm
+++ labs/lab09/lab09.scm
@@ -1,9 +1,35 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+    (cond
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        (else 1)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+    (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+    (if (= n 0)
+        (lambda (x) x)
+        (composed f (repeat f (- n 1)))))
 
-(define lst 'YOUR-CODE-HERE)
+(define lst
+    (cons   (cons 1 '())
+            (cons   2
+                    (cons   (cons   3
+                                    (cons 4 '()))
+                            (cons 5 '()))))
+)
 
-(define (without-duplicates lst) 'YOUR-CODE-HERE)
+(define (without-duplicates lst) 
+    (if (null? lst)
+        lst
+        (let ((h (car lst)))
+            (cons   h
+                (without-duplicates (filter (lambda (x) (not (= x h)))
+                                            (cdr lst)
+                                    )
+                )
+            )
+        )
+    )
+)
